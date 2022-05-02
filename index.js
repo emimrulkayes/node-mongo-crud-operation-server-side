41b55685904d8b10ea46dd2efc965d9e08415719
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-05-02 15:03:00 +0600

    update user added and deleted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -20,12 +21,29 @@ async function run() {
         await client.connect();
         const userCollection = client.db("foodExpress").collection("users");
 
+        // get users
+        app.get('/user', async(req, res) => {
+            const query = {};
+            const cursor = userCollection.find(query);
+            const users = await cursor.toArray();
+            res.send(users);
+        })
+
+        // POST User: add a new user
         app.post('/user', async(req, res) => {
             const newUser = req.body;
             console.log('added new user', newUser);
             const result = await userCollection.insertOne(newUser);
             res.send(result);
         });
+
+        // delete a user
+        app.delete('/user/:id', async(req, res) =>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const result = await userCollection.deleteOne(query);
+            res.send(result);
+        })
     }
     finally{
         // await client.close();
